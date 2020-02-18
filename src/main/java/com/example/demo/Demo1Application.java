faa21737894529c96a40cf2f8dfcd10f3d7d08f9
AsmaBanu <banusheikh1997@gmail.com>
2020-02-18 15:41:01 -0500

    jenkins demo project for practice

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		Demo1Application.run(Demo1Application.class, args);
+	}
+
+	private static void run(Class<Demo1Application> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
