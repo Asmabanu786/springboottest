79f4042d6f60326db551dd405c7378a6f7c34b1c
Asmabanu786 <59757764+Asmabanu786@users.noreply.github.com>
2020-02-24 22:43:41 -0500

    Restful web services program

---
+++ spring-web-rest-demo/src/test/java/com/cts/swrd/demo/SpringWebRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.swrd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
