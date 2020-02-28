34e77e71528478ad164cbf2effe782f1e0496045
Asmabanu786 <59757764+Asmabanu786@users.noreply.github.com>
2020-02-28 07:07:59 -0500

    Group and Contact(stage1) Program for MS

---
+++ MicroservicesContact/src/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO contacts(contact_id, first_Name,last_Name,group_Code,mail_Id,mobileNumber)
+VALUES(21, 'Sanu','Sagar','FAMILY','sanu@gmail.com', '7658983734');
---
+++ MicroservicesContact/src/main/java/com/cts/adb/MicroservicesContactApplication.java
@@ -0,0 +1,13 @@
+package com.cts.adb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesContactApplication.class, args);
+	}
+
+}
